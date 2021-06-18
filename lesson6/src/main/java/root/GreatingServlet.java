f67478e54f58a1ad9ec7ba7f2dd92862d0500f94
vasyakov <vasyakov94@gmail.com>
2021-06-18 18:00:57 +0300

    Add homework on lesson 6

---
+++ lesson6/src/main/java/root/GreatingServlet.java
@@ -0,0 +1,16 @@
+package root;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "greating_servlet", urlPatterns = "/hello")
+public class GreatingServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Hello, World!</h1>");
+    }
+}
